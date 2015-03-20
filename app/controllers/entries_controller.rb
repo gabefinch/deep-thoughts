a4b616a1aa533d45b5a23cc007cc03bbb79a27f7
Gabe Finch <gabefinch@gmail.com>
2015-03-20 10:28:07 -0700

    entries#index and bootswatch paper

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,54 @@
+class EntriesController < ApplicationController
+
+  def index
+    @entries = Entry.all
+    render :index
+  end
+
+  def new
+    @entry = Entry.new
+    @entries = Entry.all
+    render :new
+  end
+
+  def create
+    @entry = Entry.new(lesson_params)
+    if @entry.save
+      flash[:notice] = "Entry successfully added."
+      redirect_to entries_path()
+    else
+      render :new
+    end
+  end
+
+  def show
+    @entry = Entry.find(params[:id])
+  end
+
+  def edit
+    @entry = Entry.find(params[:id])
+    @entries = Entry.all
+    render :edit
+  end
+
+  def update
+    @entry = Entry.find(params[:id])
+    @entry.update_attributes(lesson_params)
+    flash[:notice] = "Entry successfully updated."
+    redirect_to entries_path
+  end
+
+  def destroy
+    @entry = Entry.find(params[:id])
+    @entry.destroy
+    flash[:notice] = "Entry successfully deleted."
+    redirect_to entries_path
+  end
+
+  private
+
+
+  def entries_params
+    params.require(:entry).permit(:title, :body)
+  end
+end
